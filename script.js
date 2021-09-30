b1afec6c80797599cb61bafd718559634be2d2a3
Kush Agarwal <83411262+Kushhhh-sh@users.noreply.github.com>
2021-09-30 17:31:12 +0530

    😄Added textbox

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ sendBtn.addEventListener("click", () => {
     sendApiRequest();
 })
 
+let text = document.querySelector('#text');
+
+
+
 async function sendApiRequest()
 {
     let appID = "09565216";
     let apiKey = "8da93543da30ab74ed025d73ce97bf27";
-    let response = await fetch(`https://api.edamam.com/api/recipe/v2?type=public&q=chicken&app_id=${appID}&app_key=${apiKey}`);
+    let food = text.value;
+    let response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${food}&app_id=${appID}&app_key=${apiKey}`);
     console.log(response);
     let data = await response.json();
     console.log(data);
