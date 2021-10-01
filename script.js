cbc090dfca22684e0d9decdac4961455e5fe1238
Kush Agarwal <83411262+Kushhhh-sh@users.noreply.github.com>
2021-10-01 17:43:55 +0530

    💯Added 3 Cards for results

--- script.js
+++ script.js
@@ -15,4 +15,34 @@ async function sendApiRequest()
     console.log(response);
     let data = await response.json();
     console.log(data);
+    addCard(data);
+}
+
+function addCard(data){
+    let canvas = document.querySelector('#main');
+    canvas.innerHTML = `
+    <div class="card" style="width: 18rem;">
+        <img src="${data.hits[0].recipe.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${data.hits[0].recipe.label}</h5>
+                <p class="card-text">Cuisine Type: ${data.hits[0].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}</p>
+                <a href="${data.hits[0].recipe.url}" class="btn btn-primary">Go somewhere</a>
+            </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="${data.hits[1].recipe.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${data.hits[1].recipe.label}</h5>
+                <p class="card-text">Cuisine Type: ${data.hits[1].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}</p>
+                <a href="${data.hits[1].recipe.url}" class="btn btn-primary">Go somewhere</a>
+            </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="${data.hits[2].recipe.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${data.hits[2].recipe.label}</h5>
+                <p class="card-text">Cuisine Type: ${data.hits[2].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}</p>
+                <a href="${data.hits[2].recipe.url}" class="btn btn-primary">Go somewhere</a>
+            </div>
+    </div> `
 }
