34e0ed113a26cb184c7c49a267df5b4057b29d4c
Kush Agarwal <83411262+Kushhhh-sh@users.noreply.github.com>
2021-10-07 21:25:22 +0530

    🍕Added script for Veg/Non-Veg

--- src/script.js
+++ src/script.js
@@ -31,7 +31,7 @@ async function sendApiRequest()
         apiLink = apiLink + `&diet=${diet}`;
 
     let response = await fetch(apiLink);
-    console.log(response);
+    // console.log(response);
     let data = await response.json();
     console.log(data);
     addCard(data);
@@ -39,12 +39,22 @@ async function sendApiRequest()
 
 function addCard(data){
     let canvas = document.querySelector('#main');
+    let vegNonVeg = new Array(12);
+    for (let i = 0; i < vegNonVeg.length; i++) {
+        if (data.hits[i].recipe.healthLabels.includes('Vegetarian')){
+            vegNonVeg[i] = 'veg.svg'
+        }else{
+            vegNonVeg[i] = 'non-veg.svg'
+        }
+        
+    }
+    // console.log(vegNonVeg);
     canvas.innerHTML = `
     <div class="card">
         <img src="${data.hits[0].recipe.image}" class="card-img-top" alt="${data.hits[0].recipe.label}">
             <div class="card-body">
                 <h5 class="card-title">${data.hits[0].recipe.label}</h5>
-                <p class="card-text">Cuisine Type: ${data.hits[0].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/non-veg.svg" alt="" class="veg-non-veg-img"></p>
+                <p class="card-text">Cuisine Type: ${data.hits[0].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/${vegNonVeg[0]}" alt="" class="veg-non-veg-img"></p>
                 <a href="${data.hits[0].recipe.url}" class="btn btn-primary" target="_blank">See Recipe</a>
             </div>
     </div>
@@ -52,7 +62,7 @@ function addCard(data){
         <img src="${data.hits[1].recipe.image}" class="card-img-top" alt="${data.hits[0].recipe.label}">
             <div class="card-body">
                 <h5 class="card-title">${data.hits[1].recipe.label}</h5>
-                <p class="card-text">Cuisine Type: ${data.hits[1].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/non-veg.svg" alt="" class="veg-non-veg-img"></p>
+                <p class="card-text">Cuisine Type: ${data.hits[1].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/${vegNonVeg[1]}" alt="" class="veg-non-veg-img"></p>
                 <a href="${data.hits[1].recipe.url}" class="btn btn-primary" target="_blank">See Recipe</a>
             </div>
     </div>
@@ -60,7 +70,7 @@ function addCard(data){
         <img src="${data.hits[2].recipe.image}" class="card-img-top" alt="${data.hits[0].recipe.label}">
             <div class="card-body">
                 <h5 class="card-title">${data.hits[2].recipe.label}</h5>
-                <p class="card-text">Cuisine Type: ${data.hits[2].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/non-veg.svg" alt="" class="veg-non-veg-img"></p>
+                <p class="card-text">Cuisine Type: ${data.hits[2].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/${vegNonVeg[2]}" alt="" class="veg-non-veg-img"></p>
                 <a href="${data.hits[2].recipe.url}" class="btn btn-primary" target="_blank">See Recipe</a>
             </div>
     </div>
@@ -68,7 +78,7 @@ function addCard(data){
         <img src="${data.hits[3].recipe.image}" class="card-img-top" alt="${data.hits[0].recipe.label}">
             <div class="card-body">
                 <h5 class="card-title">${data.hits[3].recipe.label}</h5>
-                <p class="card-text">Cuisine Type: ${data.hits[3].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/non-veg.svg" alt="" class="veg-non-veg-img"></p>
+                <p class="card-text">Cuisine Type: ${data.hits[3].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/${vegNonVeg[3]}" alt="" class="veg-non-veg-img"></p>
                 <a href="${data.hits[3].recipe.url}" class="btn btn-primary" target="_blank">See Recipe</a>
             </div>
     </div> 
@@ -76,7 +86,7 @@ function addCard(data){
         <img src="${data.hits[4].recipe.image}" class="card-img-top" alt="${data.hits[0].recipe.label}">
             <div class="card-body">
                 <h5 class="card-title">${data.hits[4].recipe.label}</h5>
-                <p class="card-text">Cuisine Type: ${data.hits[4].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/non-veg.svg" alt="" class="veg-non-veg-img"></p>
+                <p class="card-text">Cuisine Type: ${data.hits[4].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/${vegNonVeg[4]}" alt="" class="veg-non-veg-img"></p>
                 <a href="${data.hits[4].recipe.url}" class="btn btn-primary" target="_blank">See Recipe</a>
             </div>
     </div>
@@ -84,7 +94,7 @@ function addCard(data){
         <img src="${data.hits[5].recipe.image}" class="card-img-top" alt="${data.hits[0].recipe.label}">
             <div class="card-body">
                 <h5 class="card-title">${data.hits[5].recipe.label}</h5>
-                <p class="card-text">Cuisine Type: ${data.hits[5].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/non-veg.svg" alt="" class="veg-non-veg-img"></p>
+                <p class="card-text">Cuisine Type: ${data.hits[5].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/${vegNonVeg[5]}" alt="" class="veg-non-veg-img"></p>
                 <a href="${data.hits[5].recipe.url}" class="btn btn-primary" target="_blank">See Recipe</a>
             </div>
     </div>
@@ -92,7 +102,7 @@ function addCard(data){
         <img src="${data.hits[6].recipe.image}" class="card-img-top" alt="${data.hits[0].recipe.label}">
             <div class="card-body">
                 <h5 class="card-title">${data.hits[6].recipe.label}</h5>
-                <p class="card-text">Cuisine Type: ${data.hits[6].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/non-veg.svg" alt="" class="veg-non-veg-img"></p>
+                <p class="card-text">Cuisine Type: ${data.hits[6].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/${vegNonVeg[6]}" alt="" class="veg-non-veg-img"></p>
                 <a href="${data.hits[6].recipe.url}" class="btn btn-primary" target="_blank">See Recipe</a>
             </div>
     </div>
@@ -100,7 +110,7 @@ function addCard(data){
         <img src="${data.hits[7].recipe.image}" class="card-img-top" alt="${data.hits[0].recipe.label}">
             <div class="card-body">
                 <h5 class="card-title">${data.hits[7].recipe.label}</h5>
-                <p class="card-text">Cuisine Type: ${data.hits[7].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/non-veg.svg" alt="" class="veg-non-veg-img"></p>
+                <p class="card-text">Cuisine Type: ${data.hits[7].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/${vegNonVeg[7]}" alt="" class="veg-non-veg-img"></p>
                 <a href="${data.hits[7].recipe.url}" class="btn btn-primary" target="_blank">See Recipe</a>
             </div>
     </div> 
@@ -108,7 +118,7 @@ function addCard(data){
         <img src="${data.hits[8].recipe.image}" class="card-img-top" alt="${data.hits[0].recipe.label}">
             <div class="card-body">
                 <h5 class="card-title">${data.hits[8].recipe.label}</h5>
-                <p class="card-text">Cuisine Type: ${data.hits[8].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/non-veg.svg" alt="" class="veg-non-veg-img"></p>
+                <p class="card-text">Cuisine Type: ${data.hits[8].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/${vegNonVeg[8]}" alt="" class="veg-non-veg-img"></p>
                 <a href="${data.hits[8].recipe.url}" class="btn btn-primary" target="_blank">See Recipe</a>
             </div>
     </div>
@@ -116,7 +126,7 @@ function addCard(data){
         <img src="${data.hits[9].recipe.image}" class="card-img-top" alt="${data.hits[0].recipe.label}">
             <div class="card-body">
                 <h5 class="card-title">${data.hits[9].recipe.label}</h5>
-                <p class="card-text">Cuisine Type: ${data.hits[9].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/non-veg.svg" alt="" class="veg-non-veg-img"></p>
+                <p class="card-text">Cuisine Type: ${data.hits[9].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/${vegNonVeg[9]}" alt="" class="veg-non-veg-img"></p>
                 <a href="${data.hits[9].recipe.url}" class="btn btn-primary" target="_blank">See Recipe</a>
             </div>
     </div>
@@ -124,7 +134,7 @@ function addCard(data){
         <img src="${data.hits[10].recipe.image}" class="card-img-top" alt="${data.hits[0].recipe.label}">
             <div class="card-body">
                 <h5 class="card-title">${data.hits[10].recipe.label}</h5>
-                <p class="card-text">Cuisine Type: ${data.hits[10].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/non-veg.svg" alt="" class="veg-non-veg-img"></p>
+                <p class="card-text">Cuisine Type: ${data.hits[10].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/${vegNonVeg[10]}" alt="" class="veg-non-veg-img"></p>
                 <a href="${data.hits[10].recipe.url}" class="btn btn-primary" target="_blank">See Recipe</a>
             </div>
     </div>
@@ -132,7 +142,7 @@ function addCard(data){
         <img src="${data.hits[11].recipe.image}" class="card-img-top" alt="${data.hits[0].recipe.label}">
             <div class="card-body">
                 <h5 class="card-title">${data.hits[11].recipe.label}</h5>
-                <p class="card-text">Cuisine Type: ${data.hits[11].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/non-veg.svg" alt="" class="veg-non-veg-img"></p>
+                <p class="card-text">Cuisine Type: ${data.hits[11].recipe.cuisineType}<br>Dish Type: ${data.hits[2].recipe.dishType}<br>Meal Type: ${data.hits[2].recipe.mealType}<br>Veg/Non Veg: <img src="src/${vegNonVeg[11]}" alt="" class="veg-non-veg-img"></p>
                 <a href="${data.hits[11].recipe.url}" class="btn btn-primary" target="_blank">See Recipe</a>
             </div>
     </div> 
